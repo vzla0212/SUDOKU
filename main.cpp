c25d709240e1b0f3541d899b97fd5a9905701ef1
omar <elgordo18635985@gmail.com>
2012-11-18 12:17:46 -0430

    hola

--- main.cpp
+++ main.cpp
@@ -15,6 +15,11 @@ using namespace std;
  */
 int main(int argc, char** argv) {
     std::cout << "working";
+    system("./minisat/build/release/bin/minisat simple_v3_c2.cnf salida.txt");
+    
+    
+    
+    //printf("This is a directory list...");
     return 0;
 }
 
